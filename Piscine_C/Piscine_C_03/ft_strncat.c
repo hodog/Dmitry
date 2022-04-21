547bbe215c5dbab64a5de751286c5843387cbf3c
lalfred <dm.blv@yandex.ru>
2022-04-21 21:32:27 +0300

    Piscine03 update

--- Piscine_C/Piscine_C_03/ft_strncat.c
+++ Piscine_C/Piscine_C_03/ft_strncat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lalfred <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/19 22:24:16 by lalfred           #+#    #+#             */
-/*   Updated: 2022/04/21 17:07:37 by lalfred          ###   ########.fr       */
+/*   Updated: 2022/04/21 17:07:48 by lalfred          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ int	ft_strlen(char *str)
 	return (i);
 }
 
-char	*ft_strcat(char *dest, char *src)
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int	i;
 	unsigned int	n;
 
 	i = 0;
 	n = ft_strlen(dest);
-	while (src[i])
+	while (src[i] && i < nb)
 	{
 		dest[n] = src[i];
 		i++;
@@ -49,17 +49,18 @@ int	main(void)
 	char	s2[] = "abcd";
 	char	*s3;
 	char	s4[] = "abcd";
+	int		nb = 3;
 	
 	s1 = (char *)malloc(sizeof(*s1) * (ft_strlen(s2) + 1));
 	s3 = (char *)malloc(sizeof(*s3) * (ft_strlen(s4) + 1));
 
 	printf("========							========\n");
-	strcat(s1, s2);
-	ft_strcat(s3, s4);
+	strncat(s1, s2, nb);
+	ft_strncat(s3, s4, nb);
 	ft_strlen(s1) == ft_strlen(s3) ? printf("				Correct\n") : printf("				 Error\n");
 	printf("========							========\n");
-	strcat(s1, s2);
-	ft_strcat(s3, s4);
+	strncat(s1, s2, nb);
+	ft_strncat(s3, s4, nb);
 	ft_strlen(s1) == ft_strlen(s3) ? printf("				Correct\n") : printf("				 Error\n");
 	printf("========							========\n");
 	printf("string length : 		Original -> %d - %d <- Reproduced \n", ft_strlen(s1), ft_strlen(s3));
---
+++ Piscine_C/Piscine_C_03/ft_strstr.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lalfred <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 17:08:19 by lalfred           #+#    #+#             */
+/*   Updated: 2022/04/21 19:50:25 by lalfred          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strncat_rev(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	n;
+
+    n = 0;
+	while (src[nb])
+	{
+		dest[n] = src[nb];
+        printf("dest - %c\n", dest[n]);
+		nb++;
+		n++;
+	}
+	dest[n] = 0;
+    printf("dest - %s\n", dest);
+	return (dest);
+}
+
+int ft_comparison(char *str, char *to_find, unsigned int n)
+{
+    int j;
+
+    j = 0;
+    while (str[n] == to_find[j])
+    {
+        n++;
+        j++;
+        if (!to_find[j])
+            return(1);
+    }
+    return (0);
+}
+
+char    *ft_strstr(char *str, char *to_find)
+{
+    unsigned int    i;
+    unsigned int    j;
+    int             flag;
+    char            *haystack;
+
+    i = 0;
+    flag = 0;
+    if (to_find[0] == '\0')
+        return (str);
+    while (str[i])
+    {
+        flag = ft_comparison(str, to_find, i);
+        printf("flag = %d\n", flag);
+        if (flag == 1)
+            {
+                ft_strncat_rev(haystack, str, i);
+                printf("haystack - %s\n", haystack);
+                                printf("str - %s\n", str);
+                return(haystack);
+            }
+        i++;
+    }    
+    return (0);
+}
+
+// Check :
+
+#include <string.h>
+
+int main(void)
+{
+    char    haystack[] = "Hello World!";
+    char    needle[] = "l";
+
+
+//    printf("%s\n", strstr(haystack, needle));
+    printf("%s\n", ft_strstr(haystack, needle));
+    
+    return (0);
+}
