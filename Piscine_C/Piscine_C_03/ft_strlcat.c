d3aba19ab3774793c8348d14d056f60fcd3fb242
hodog <dm.blv@yandex.ru>
2022-04-26 23:31:06 +0300

    update

--- Piscine_C/Piscine_C_03/ft_strlcat.c
+++ Piscine_C/Piscine_C_03/ft_strlcat.c
@@ -6,9 +6,11 @@
 /*   By: lalfred <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/23 11:27:06 by lalfred           #+#    #+#             */
-/*   Updated: 2022/04/25 15:35:42 by lalfred          ###   ########.fr       */
+/*   Updated: 2022/04/26 23:28:05 by lalfred          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 int	ft_strlen(char *str)
 {
@@ -24,19 +26,23 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	n;
+	unsigned int	length;
 
 	i = 0;
 	if (size == 0)
 		return (ft_strlen(src));
 	n = ft_strlen(dest);
-	while (i < size - ft_strlen(dest) - 1 && size < n)
+	length = n;
+	while (i < size - length - 1 && src[i])
 	{
 		dest[n] = src[i];
 		i++;
 		n++;
 	}
 	dest[n] = 0;
-	return (ft_strlen(dest));
+	if (size > length)
+		return (length + ft_strlen(src));
+	return (length);
 }
 
 // Check :
@@ -48,10 +54,10 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 int	main(void)
 {
 	char	*s1;
-	char	s2[] = "abcd";
+	char	s2[] = "10101010101";
 	char	*s3;
-	char	s4[] = "abcd";
-	int		nb = 10;
+	char	s4[] = "10101010101";
+	int		nb = 100;
 	
 	s1 = (char *)malloc(sizeof(*s1) * (ft_strlen(s2) + 1));
 	s3 = (char *)malloc(sizeof(*s3) * (ft_strlen(s4) + 1));
@@ -61,18 +67,18 @@ int	main(void)
 	printf("s3 : %s	s4 : %s : %d\n", s3, s4, nb);
 	printf("		========****************************========\n");
 	strlcat(s1, s2, nb);
-	printf("srtlcat : %u\n", strlcat(s1, s2, nb));
 	ft_strlcat(s3, s4, nb);
-	printf("ft_srtlcat : %d\n", ft_strlcat(s3, s4, nb));
+	printf("strlcat : %lu\n", strlcat(s1, s2, nb));
+	printf("ft_strlcat : %d\n", ft_strlcat(s3, s4, nb));
 	printf("s1 : %s	s2 : %s : %d\n", s1, s2, nb);
 	printf("s3 : %s	s4 : %s : %d\n", s3, s4, nb);
 	printf("		========****************************========\n");
-	strlcat(s1, s2, nb);
 	printf("s1 : %s	s2 : %s : %d\n", s1, s2, nb);
 	printf("s3 : %s	s4 : %s : %d\n", s3, s4, nb);
-	printf("srtlcat : %u\n", strlcat(s1, s2, nb));
+	strlcat(s1, s2, nb);
 	ft_strlcat(s3, s4, nb);
-	printf("ft_srtlcat : %d\n", ft_strlcat(s3, s4, nb));
+	printf("strlcat : %lu\n", strlcat(s1, s2, nb));
+	printf("ft_strlcat : %d\n", ft_strlcat(s3, s4, nb));
 	printf("s1 : %s	s2 : %s : %d\n", s1, s2, nb);
 	printf("s3 : %s	s4 : %s : %d\n", s3, s4, nb);
 	printf("========							========\n");
