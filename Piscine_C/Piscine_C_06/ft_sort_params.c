7cbc4a3b57503ff10db9251e192ba1f27de40b47
lalfred <dm.blv@yandex.ru>
2022-05-04 15:26:51 +0300

    Piscine_C_06 added, ft_strcmp updated

--- Piscine_C/Piscine_C_06/ft_sort_params.c
+++ Piscine_C/Piscine_C_06/ft_sort_params.c
@@ -6,44 +6,72 @@
 /*   By: lalfred <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 10:06:49 by lalfred           #+#    #+#             */
-/*   Updated: 2022/05/03 21:44:45 by lalfred          ###   ########.fr       */
+/*   Updated: 2022/05/04 15:25:53 by lalfred          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int	ft_sort(char **str)
+void	ft_putchar(char c)
 {
-	int	i;
-	int	j;
-	int	n;
-	int	m;
-	
-	if (str[n][i] < str[m][j])
+	write (1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		
+		ft_putchar(str[i]);
+		i++;
 	}
 }
 
-int	main(int argc, char **argv)
+int		ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-	int	n;
-	
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_print_params(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int		i;
+	char	*tmp;
+
+	i = 1;
 	if (argc > 1)
 	{
-		n = ft_sort(*argv[n]);
-		while (argv[n])
+		while (i < argc - 1)
 		{
-			i = 0;
-			while (argv[n][i])
+			if (ft_strcmp(argv[i], argv[i + 1]) > 0)
 			{
-				write(1, &argv[n][i], 1);
-				i++;
+				tmp = argv[i];
+				argv[i] = argv[i + 1];
+				argv[i + 1] = tmp;
+				i = 0;
 			}
-			write(1, "\n", 1);
-			n++;
+			i++;
 		}
+		ft_print_params(argc, argv);
 	}
 	return (0);
 }
